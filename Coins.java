e6bf53839cef758136a0928f582ef1dc0fdef067
Jamiee-N <jpnguyen124@student.hbuhsd.edu>
2024-09-20 11:17:03 -0700

    coins

--- Coins.java
+++ Coins.java
@@ -2,9 +2,30 @@ public class Coins
 {
   public static void main(String[] args)
   {
-    // test code here
+    coinConverter(250);
   }
 
-  // write code for coinConverter
+  public static void coinConverter(int pennies) 
+  {
+    int dollars = pennies/100;
+    pennies %= 100;
+
+    int quarters = pennies/25;
+    pennies %= 25;
+
+    int dimes = pennies/10;
+    dimes %= 10;
+
+    int nickels = pennies/5;
+    nickels %= 5;
+
+    int leftover = nickels;
+
+   System.out.println("Dollars:" + dollars);
+   System.out.println("Quarters:" + quarters);
+   System.out.println("Dimes:" +  dimes);
+   System.out.println("Nickels:" + nickels);
+   System.out.println("Pennies:" + leftover);
+  }
 
 }
